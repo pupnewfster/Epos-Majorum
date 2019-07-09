05deefca2826f7c49ae7ccf2f7fa64e8744f44f4
pupnewfster <richard@freimer.com>
2019-07-08 20:54:00 -0400

    Initial port of being able to register/retrieve locks. - Temporary IRequirement object added so that locks have a "type" they can reference what they are mapped to - GenericLockKey still needs to be reworked but there is a TODO on how

--- src/api/java/com/teamacronymcoders/eposmajorum/api/locks/IParentLockKey.java
+++ src/api/java/com/teamacronymcoders/eposmajorum/api/locks/IParentLockKey.java
@@ -1,5 +1,9 @@
 package com.teamacronymcoders.eposmajorum.api.locks;
 
+import com.teamacronymcoders.eposmajorum.api.requirements.IRequirement;
+import java.util.List;
+import javax.annotation.Nonnull;
+
 public interface IParentLockKey extends ILockKey {
     /**
      * Retrieves any sub requirements this key may have.
@@ -7,6 +11,8 @@ public interface IParentLockKey extends ILockKey {
      *
      * @return A RequirementHolder of the sub requirements.
      */
-    //List<RequirementHolder> getSubRequirements();
-    //TODO: Make this return a list so that it can combine them on a higher level using whatever specific logic handler it wants to use
+    @Nonnull
+    List<IRequirement> getSubRequirements();
+    //TODO: Update javadocs reason this is a list is so that the higher tier logic handlers can decide how to merge ALL the different requirements together
+    // This is not a set in case something wants to know if there are duplicates and merge them differently
 }
---
+++ src/api/java/com/teamacronymcoders/eposmajorum/api/requirements/IRequirement.java
@@ -0,0 +1,6 @@
+package com.teamacronymcoders.eposmajorum.api.requirements;
+
+//TODO: Actually implement this and other stuff, this package and this interface are currently
+// Just placeholders so that the lock logic has something to reference
+public interface IRequirement {
+}
