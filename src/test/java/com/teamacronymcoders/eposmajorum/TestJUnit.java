98e3449f77a01640e63339bc20d31e5872e93627
pupnewfster <richard@freimer.com>
2019-07-11 16:04:40 -0400

    Setup JUnit

---
+++ src/test/java/com/teamacronymcoders/eposmajorum/TestJUnit.java
@@ -0,0 +1,14 @@
+package com.teamacronymcoders.eposmajorum;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TestJUnit {
+
+    @Test
+    public void test() {
+        //Ensure JUnit is working properly
+        assertTrue(true);
+    }
+}
